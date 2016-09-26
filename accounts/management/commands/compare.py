b96d6ad7363bdaac0a2cb110cf11b62277863839
Alan Bernstein <abernstein@umbel.com>
2016-09-26 02:04:58 -0500

    complete compare command, just output to stdout

--- accounts/management/commands/compare.py
+++ accounts/management/commands/compare.py
@@ -1,13 +1,6 @@
-import re
-import os
-from collections import Counter
-import datetime
-from itertools import groupby
-import matplotlib.pyplot as plt
+import json
+# import matplotlib.pyplot as plt
 from django.core.management.base import BaseCommand
-from django.db.models import Sum
-from taggit.models import Tag
-from accounts.models import Transaction, Merchant
 from accounts.base import get_account
 from accounts.views import get_compare_data
 
@@ -19,30 +12,30 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--account', '-a', default='', help='specify account to use')
         parser.add_argument('--tags', '-t', default='', help='tags')
+        parser.add_argument('--merchants', '-m', default='', help='merchants')
 
     def handle(self, *args, **options):
-
         if options['account']:
             account = get_account(options['account'])
 
-        tags = options['tags']
-        data = get_compare_data(tags)
-        print('%s, %s, %s' % ('x', 'y1', 'y2'))
-        for row in zip(data['x'], data['y1'], data['y2']):
-            print('%s, %s, %s' % row)
-        plot_compare_data(data)
+        # get inputs
+        if options['merchants']:
+            merchants = [x.strip() for x in options['merchants'].split(',')]
+            data = get_compare_data(merchants=merchants)
+        if options['tags']:
+            tags = [x.strip() for x in options['tags'].split(',')]
+            data = get_compare_data(tags=tags)
 
+        print(json.dumps(data))
 
-def plot_compare_data(data):
-    plt.plot(data['x'], data['y1'], 'r')
-    plt.plot(data['x'], data['y2'], 'b')
-    plt.show()
-    pass
-
-
-def get_compare_data(tags):
-    data = {'x': [0, 1, 2],
-            'y1': [0, 1, 4],
-            'y2': [0, 1, 2],
-            }
-    return  data
+        """
+        # collate and print
+        keys = data.keys()
+        header = ', '.join(keys)
+        print(header)
+        for n in range(len(data[keys[0]])):
+            m = []
+            for key in keys:
+                m.append(str(data[key][n]))
+            print(', '.join(m))
+        """
