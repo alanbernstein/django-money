81b94e8fc1e6385535d24969a089a219974fccb2
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2017-03-21 17:13:15 -0500

    reorganize urls

--- money/urls.py
+++ money/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from accounts.views.main import index
-from accounts.views import transactions, merchants, statements, accts, tags
+from accounts.views import transactions, merchants, statements, accts, tags, graphs
 
 urlpatterns = [
     url(r'^api/', include('api.v1.urls')),  # not sure about this
@@ -27,19 +27,19 @@ urlpatterns = [
     url(r'^transactions$', transactions.transaction_list, name='transaction-list'),
     url(r'^transactions/(?P<transaction_id>[0-9]+)$', transactions.transaction_detail, name='transaction-detail'),
     url(r'^transactions/untagged$', transactions.transactions_untagged, name='transactions-untagged'),
-    url(r'^transactions/compare/(?P<tags>.*)$', transactions.transactions_compare, name='transactions-compare'),
-    url(r'^transactions/subdivide/(?P<tags>.*)$', transactions.transactions_subdivide, name='transactions-subdivide'),
     url(r'^merchants$', merchants.merchant_list, name='merchant-list'),
     url(r'^merchants/(?P<merchant_id>[0-9]+)$', merchants.merchant_detail, name='merchant-detail'),
     url(r'^merchants/untagged$', merchants.merchants_untagged, name='merchants-untagged'),
     url(r'^merchants/unnamed$', merchants.merchants_unnamed, name='merchants-unnamed'),
-    # url(r'^timeseries$', accounts.vws.timeseries, name='timeseries'),
-    # url(r'^trends', accounts.vws.trends, name='trends'),
     url(r'^accounts$', accts.account_list, name='account-list'),
     url(r'^accounts/(?P<account_id>[0-9]+)$', accts.account_detail, name='account-detail'),
     url(r'^statements$', statements.statement_list, name='statement-list'),
     url(r'^statements/(?P<statement_id>[0-9]+)$', statements.statement_detail, name='statement-detail'),
-
+    url(r'^compare/(?P<tags>.*)$', graphs.compare, name='graph-compare'),
+    # url(r'^timeseries$', accounts.vws.timeseries, name='timeseries'),
+    # url(r'^trends', accounts.vws.trends, name='trends'),
+    # url(r'^transactions/compare/(?P<tags>.*)$', transactions.transactions_compare, name='transactions-compare'),
+    # url(r'^transactions/subdivide/(?P<tags>.*)$', transactions.transactions_subdivide, name='transactions-subdivide'),
 ]
 
 # (?P<question_id>[0-9]+)
